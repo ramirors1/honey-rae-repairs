be2db703f0e75ba6872b08a6e024b2ff8cc0378d
Your Github User Name <ramirezr@protonmail.com>
2021-11-23 16:01:13 -0600

    working on chap15

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -6,17 +6,35 @@ import "./Tickets.css"
 export const TicketList = () => {
     const [tickets, updateTickets] = useState([])
     const history = useHistory()
+    const getAllTickets = () => {
+        return fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
+        .then(res => res.json())
+        .then((data) => {
+            updateTickets(data)
+        })
+       } 
+
 
     useEffect(
-        () => {
-            fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
-                .then(res => res.json())
-                .then((data) => {
-                    updateTickets(data)
-                })
+        () => {getAllTickets()
+                
         },
         []
     )
+            const deleteTicket = (id) => {
+                fetch(`http://localhost:8088/serviceTickets/${id}`, {
+                method: "DELETE"
+            })
+            getAllTickets()
+
+        }
+        //    const getAllTickets = () => {
+        //     return fetch("http://localhost:8088/serviceTickets?_expand=employee&_expand=customer")
+        //     .then(res => res.json())
+        //     .then((data) => {
+        //         updateTickets(data)
+        //     })
+        //    } 
 
     return (
         <>
@@ -30,15 +48,21 @@ export const TicketList = () => {
                             {/* //ternary statement, if ticket is emergency then className is emergency, else className is ticket */}
                             <p className={ticket.emergency ? "emergency ticket" : "ticket"}>  
                             {ticket.emergency ? "🚑" : ""} <Link to={`/tickets/${ticket.id}`}>{ticket.description}</Link> submitted by {ticket.customer.name} and worked on by {ticket.employee.name}
+                             <button onClick={() => {deleteTicket(ticket.id)}}>Delete</button>
                             </p>
                             {/* <p>{ticket.description} submitted by {ticket.customer.name}
                             and worked on by {ticket.employee.name}</p> */}
                             </div>
                     }
                 )
-            }
+                
+                    }
+                
+            
         </>
     )
 }
+{/* <button onClick={() => history.push("/employee/create")}>Hire Employee</button> */}
 
+{/* <button onClick={() => history.push("/ticketList")}>Delete</button> */}
 
